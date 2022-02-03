8b7757e9747b0057d38ebc7fd08351856a8e2450
Hugoacr <hugoacr@yahoo.com.br>
2022-02-03 15:05:38 -0300

    cria query que exiba o nome e a quantas vezes foi tocada pelos usuários dos planos gratuito/pessoal

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+SELECT
+    S.song_name AS nome,
+    COUNT(DISTINCT US.user_id) AS reproducoes
+FROM Song AS S
+	INNER JOIN User_Song AS US
+ON S.song_id = US.reproduction_history
+	INNER JOIN User AS U
+ON US.user_id = U.user_id
+	INNER JOIN Plan AS P
+ON U.plan_id = P.plan_id
+WHERE P.plan_name = "gratuito" OR P.plan_name = "pessoal"
+GROUP BY nome
+ORDER BY reproducoes DESC;
